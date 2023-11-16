a34383890fb7f4f88ccc681e300d6f89a74b15b8
PutraNugraha-new <putranugraha803@gmail.com>
2023-11-17 00:45:46 +0700

    penyempurnaan fitur fitur dan hak akses

---
+++ application/models/M_all.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_all extends CI_Model {
+
+    // ambil data 
+    public function countTps(){
+        $this->db->select('*');
+        $this->db->from('tps');
+        return $this->db->count_all_results();
+    }
+    public function countKab(){
+        $this->db->select('*');
+        $this->db->from('kabupaten');
+        return $this->db->count_all_results();
+    }
+    public function countKec(){
+        $this->db->select('*');
+        $this->db->from('kecamatan');
+        return $this->db->count_all_results();
+    }
+    public function countKel(){
+        $this->db->select('*');
+        $this->db->from('kelurahan');
+        return $this->db->count_all_results();
+    }
+}
+
+/* End of file M_Wisata.php */
+/* Location: ./application/models/M_Wisata.php */
